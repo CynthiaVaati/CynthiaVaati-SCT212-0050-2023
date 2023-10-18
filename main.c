4996fc58ce2c78a6f4006effc567c0840cdfee6e
CynthiaVaati <146328564+CynthiaVaati@users.noreply.github.com>
2023-10-18 23:35:46 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+float temperature;
+printf("Enter your temperature: ");
+scanf("%f",&temperature);
+
+printf("CONVERSION OF TEMPERATURE FROM FARENHEIT TO CELCIUS\n");
+
+while (temperature < 100)
+
+{
+float celcius;
+celcius=(temperature-32)*5/9;
+printf("%.2f fareheit = %.2f degrees\n",temperature,celcius);
+temperature=temperature+10;
+}
+return 0;
+}
