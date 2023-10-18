af240d677a8e2ebdac083349d2b911de91f6a7ed
CynthiaVaati <146328564+CynthiaVaati@users.noreply.github.com>
2023-10-18 23:26:10 +0300

    Update temperatureconversion.c
    added the new line function

--- temperatureconversion.c
+++ temperatureconversion.c
@@ -6,14 +6,14 @@ float temperature;
 printf("Enter your temperature: ");
 scanf("%f",&temperature);
 
-printf("CONVERSION OF TEMPERATURE FROM FARENHEIT TO CELCIUS");
+printf("CONVERSION OF TEMPERATURE FROM FARENHEIT TO CELCIUS\n");
 
 while (temperature < 100)
 
 {
 float celcius;
 celcius=(temperature-32)*5/9;
-printf("%.2f fareheit = %.2f degrees/n",temperature,celcius);
+printf("%.2f fareheit = %.2f degrees\n",temperature,celcius);
 temperature=temperature+10;
 }
 return 0;
